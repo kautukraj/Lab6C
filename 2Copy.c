808eb0a9498b431e6806b14f1f2bd7bd9d2e8c81
Kautuk Raj <spamkraj@gmail.com>
2019-09-18 21:08:16 +0530

    Add files via upload

---
+++ 2Copy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int i=0,flag=0;
+    char *p1 = (char*) malloc (255*sizeof(char));
+    char *p2 = (char*) malloc (255*sizeof(char));
+    scanf("%[^' '] %s",p1,p2);
+    while (*(p1+i)!='\0' || *(p2+i)!='\0')
+    {
+        if (*(p1+i)!=*(p2+i))
+        {flag=1;break;}
+        i++;
+    }
+    printf("%d\n",flag);
+    free(p1);
+    free(p2);
+    return 0;
+}
+
--- 3.c
+++ 3.c
@@ -1,36 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
- int findlen(const char * s)
- {
-   int l = 0;
-   while ( s[l] ) ++l;
-   return l;
- }
-
- int main()
- {
-   const char *s1 = "krishna";
-   const char *s2 = "Prasad";
-
-   int l1 = my_strlen(s1);
-   int l2 = my_strlen(s2);
-
-   char * s3 = (char *) malloc(l1 + l2 + 1); // make room for the string terminator
-
-   int n1, n2, n3;
-   n3 = 0;
-   for (n1=0; n1 < l1; ++n1, ++n3)
-     s3[n3] = s1[n1];
-
-   for (n2=0; n2 < l2; ++n2, ++n3)
-     s3[n3] = s2[n2];
-
-   s3[n3] = '\0';
-
-   printf("string 3 is %s\n", s3);
-
-   free(s3);
-
-   return 0;
+int my_own_strlen(char* p)
+{
+    int l = 0;
+    while (p[l])
+        ++l;
+    return l;
 }
+
+int main()
+{
+    int i = 0, flag = 0;
+    int j = 0, k = 0;
+    char* p1 = (char*)malloc(255 * sizeof(char));
+    char* p2 = (char*)malloc(255 * sizeof(char));
+    scanf("%[^' '] %s", p1, p2);
+    int l1 = my_own_strlen(p1);
+    int l2 = my_own_strlen(p2);
+    char* p3 = (char*)malloc(l1 + l2 + 1);
+
+    while (i < l1)
+    {
+        p3[k] = p1[i];
+        k++;
+        i++;
+    }
+
+    while (j < l2)
+    {
+        p3[k] = p2[j];
+        k++;
+        j++;
+    }
+    p3[k] = '\0';
+
+    printf("%s\n", p3);
+    free(p1);
+    free(p2);
+    free(p3);
+    return 0;
+}
+
